8933fad439fd063cbe9f625b2d5798a2656ccc9f
DougMontas <dougmontas@gmail.com>
2022-04-16 23:00:17 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,33 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+// import { render } from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+// import { Home } from "./component/home.jsx";
+// import { NavBar } from "./component/home.jsx";
+// import { JumboTron } from "./component/home.jsx";
+// import { Cards } from "./component/home.jsx";
+// import { PageFooter } from "./component/home.jsx";
 
-render(
-    <div>
-        <NavBar />,
-        <JumboTron />,
-        <Cards />,
-        <PageFooter />,
-    </div>
-)
+// import React, { Home } from 'react';
+// import React, { NavBar } from 'react';
+// import React, { JumboTron } from 'react';
+// import React, { Cards } from 'react';
+// import React, { PageFooter } from 'react';
+import { NavBar, JumboTron, Cards, PageFooter } from "./component/home.jsx";
 
+<div>
 
-
-
-
-
+	<NavBar />,
+	<JumboTron />,
+	<Cards />,
+	<PageFooter />, 
+    
+</div>;
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
